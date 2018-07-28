88b6e90c75e55f7fd71e6fcab478909a1b7127c7
Sameer Sarmah <sameerparansarmah@gmail.com>
2018-07-28 19:15:29 +0530

    refactoring

--- src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const FETCH_RECIPES = 'fetch_recipes';
-export const FETCH_COUNT = 'fetch_count';
-export const FETCH_RECIPE='fetch_recipe';
-export const SELECT_RECIPE='select_recipe';
-export const ADD_TO_CART='add_to_cart';
-export const DELETE_FROM_CART='delete_from_cart';
-export const GET_RECIPES_IN_CART='get_recipes_in_cart';
